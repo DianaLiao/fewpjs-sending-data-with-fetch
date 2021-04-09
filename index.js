ff409572c353d3b306e64ddfc7b8e57a94c2c842
Diana Liao <dijali@gmail.com>
2021-04-08 21:11:25 -0400

    done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+
+
+function submitData (nameStr, emailStr) {
+  const formData = {name: nameStr, email: emailStr}
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  }
+  const body = document.querySelector("body")
+  return fetch("http://localhost:3000/users", configObj) 
+    .then (response => response.json())
+    .then (object => {
+      body.append(object.id)
+    })
+    .catch (error => {
+      alert("WHYYYYY")
+      body.append(error.message)
+    })
+}
